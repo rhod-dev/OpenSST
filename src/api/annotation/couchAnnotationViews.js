fd3e95238b3f79891f57d683c99262d98afe01b7
Scott Bell <scott@traclabs.com>
2022-02-03 11:17:08 +0100

    modify views

--- src/api/annotation/couchAnnotationViews.js
+++ src/api/annotation/couchAnnotationViews.js
@@ -20,16 +20,15 @@ function byTargetID(doc) {
     }
 }
 
-function byTargetEndTime(doc) {
-  if (doc.model.type === "annotation") {
-      const targetKeys = Object.keys(doc.model.targets);
-      for (i = 0; i < targetKeys.length; i += 1) {
-          const targetName = targetKeys[i];
-          const target = doc.model.targets[targetName];
-          if (target.targetTime) {
-              const start = parseInt(target.targetTime.start);
-              emit([targetName, start, doc._id], null);
-          }
-      }
-  }
+function byTargetStartTime(doc) {
+    if (doc.model.type === "annotation") {
+        const targetKeys = Object.keys(doc.model.targets);
+        for (let i = 0; i < targetKeys.length; i += 1) {
+            const targetName = targetKeys[i];
+            const target = doc.model.targets[targetName];
+            if (target.targetTime) {
+                emit([targetName, target.targetTime.start, doc._id], null);
+            }
+        }
+    }
 }
