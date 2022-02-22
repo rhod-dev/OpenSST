0834625b71ff714a13a6ff27b8b59f3dd0c3ed45
Scott Bell <scott@traclabs.com>
2022-02-22 13:55:55 +0100

    adding big search bar

---
+++ src/ui/layout/GrandSearch.vue
@@ -0,0 +1,75 @@
+<template>
+<search
+    ref="shell-search"
+    class="c-search"
+    :value="searchValue"
+    @input="searchEverything"
+    @clear="searchEverything"
+/>
+</template>
+
+<script>
+import search from '../components/search.vue';
+
+export default {
+    name: 'GrandSearch',
+    components: {
+        search
+    },
+    inject: ['openmct'],
+    props: {
+    },
+    data() {
+        return {
+            searchValue: ''
+        };
+    },
+    computed: {
+    },
+    methods: {
+        searchEverything(value) {
+            // if an abort controller exists, regardless of the value passed in,
+            // there is an active search that should be canceled
+            if (this.abortSearchController) {
+                this.abortSearchController.abort();
+                delete this.abortSearchController;
+            }
+
+            this.searchValue = value;
+            this.searchLoading = true;
+
+            if (this.searchValue !== '') {
+                // clear any previous search results
+                this.searchResultItems = [];
+
+                this.getSearchResults();
+            } else {
+                this.searchLoading = false;
+            }
+        },
+        getSearchResults() {
+            // an abort controller will be passed in that will be used
+            // to cancel an active searches if necessary
+            this.abortSearchController = new AbortController();
+            const abortSignal = this.abortSearchController.signal;
+            const searchPromises = this.openmct.objects.search(this.searchValue, abortSignal);
+
+            searchPromises.map(promise => promise
+                .then(results => {
+                    return;
+                }
+                ));
+
+            Promise.all(searchPromises).catch(reason => {
+                // search aborted
+            }).finally(() => {
+                this.searchLoading = false;
+
+                if (this.abortSearchController) {
+                    delete this.abortSearchController;
+                }
+            });
+        }
+    }
+};
+</script>
